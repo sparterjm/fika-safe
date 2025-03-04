46b3f1b46ad9bdb3a0c766b8ac81045c5b63a68a
sparterjm <114603148+sparterjm@users.noreply.github.com>
2025-03-04 08:48:11 +0300

    Update page.tsx

--- app/(pages)/users/page.tsx
+++ app/(pages)/users/page.tsx
@@ -1,9 +1,9 @@
-'use client'
-import {Button} from '@/app/Components/ui/button';
+'use client';
+import { Button } from '@/app/Components/ui/button';
 import CrimeMap from '@/app/Components/Crime';
 import { IncidentReport } from '@/app/Components/incident-report';
 import clsx from 'clsx';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { XIcon } from 'lucide-react';
 
 export default function UserPage() {
@@ -12,19 +12,27 @@ export default function UserPage() {
   const toggleForm = () => {
     setOpenForm((prev) => !prev);
   };
+
   return (
-    <div className='w-[100vw] h-screen items-center flex overflow-hidden'>
-      <div className={clsx(openForm ? 'w-3/5' : 'w-full', 'duration-300 transition-all')}>
+    <div className='w-full h-full flex items-center'>
+      {/* Main Content with Map */}
+      <div className={clsx(openForm ? 'w-3/5' : 'w-full', 'transition-all duration-300')}>
         <CrimeMap />
-        <div className='absolute top-20 right-4'>
-          <Button onClick={toggleForm} >
-            {openForm ? 
-            <XIcon className='w-[1.2rem] h-[1.2rem]'/> : 'Open Form'}
+        <div className='fixed top-20 right-4 z-50'>
+          <Button onClick={toggleForm} aria-label={openForm ? 'Close Form' : 'Open Form'}>
+            {openForm ? <XIcon className='w-[1.2rem] h-[1.2rem]' /> : 'Open Form'}
           </Button>
         </div>
       </div>
-      <div className={clsx(`w-1/5 h-full ml-5 mt-5`, !openForm && 'hidden w-0')}>
-        <IncidentReport />
+
+      {/* Incident Report Form */}
+      <div
+        className={clsx(
+          'h-full ml-5 mt-5 transition-all duration-300',
+          { 'hidden': !openForm, 'w-1/4': openForm } // Adjusted width for better usability
+        )}
+      >
+        {openForm && <IncidentReport />}
       </div>
     </div>
   );
